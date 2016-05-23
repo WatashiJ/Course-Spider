183d0c55f0e4f1171104fc7b2aed179c0a044ce4
WatashiJ <yx299612@dal.ca>
2016-05-23 17:49:01 -0300

    Much more robust, and compatible with different formats from Commerce. Add professor to the json. Fixed the bug of getting subject list

--- courses.py
+++ courses.py
@@ -15,10 +15,10 @@ class DalCourse:
 		self.credit = 0
 		self.time = "NONE"
 		self.address = "DALHOUSIE"
-		self.maxStudent = 0
-		self.current = 0
-		self.available = 0
-		self.wList = 0
+		self.maxStudent = "0"
+		self.current = "0"
+		self.available = "0"
+		self.wList = "0"
 		self.percentage = "%0"
 		self.weekdays = [False, False, False, False, False]
 		self.Labs = []
@@ -43,14 +43,14 @@ class DalCourse:
 		lab.credit = 0
 		lab.time = info[23].string
 		lab.address = info[25].string
-		lab.maxStudent = int(info[27].string)
-		lab.current = int(info[29].string)
-		lab.available = int(info[31].string)
+		lab.maxStudent = info[27].text
+		lab.current = info[29].text
+		lab.available = info[31].text
 		if info[33].string != '\xa0':
-			lab.wList = int(info[33].string)
+			lab.wList = info[33].string
 		else:
 			lab.wList = 0
-		lab.percentage = info[35].font.string.split('\n')[0]
+		lab.percentage = info[35].font.string.split('\n')[0].replace(" ", "")
 		lab.professor = "Staff"
 		for i in range(13,22):
 			if info[i].string != "\xa0" and info[i] != "\n":
@@ -73,6 +73,7 @@ class DalCourse:
 		courseDict["wList"] = self.wList
 		courseDict["percentage"] = self.percentage
 		courseDict["weekdays"] = self.weekdays
+		courseDict["professor"] = self.professor
 		if len(self.Labs) != 0:
 			courseDict["Labs"] = []
 			for eachLab in self.Labs:
@@ -82,20 +83,18 @@ class DalCourse:
 		return courseDict
 
 class courseSpider:
+	data = ""
 	def __init__(self):
 		self.courses = set()
-		self.data = ""
 		self.pages = []
-		self.gatherTerms()
-		self.gatherSubject()
 		
 	def gatherPages(self, subject = "CSCI", term = "201710"):
 		url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term=" + term + "&s_subj=" + subject + "&s_numb=&n=1&s_district=All"
-		data = self.gatherData(url)
+		courseSpider.data = self.gatherData(url)
 		pattern = re.compile('^(<center>Page\s*<b>1<\/b>\s*)(.*\s*?)*?<\/center>$', re.M)
-		result = re.search(pattern, data)
+		result = re.search(pattern, courseSpider.data)
+		self.pages = ["fysktime.P_DisplaySchedule?s_term="+ term + "&s_subj=" + subject + "&s_numb=&n=1&s_district=All"]
 		if result is None:
-			self.pages = ["fysktime.P_DisplaySchedule?s_term=201710&s_subj=" + self.subject + "&s_numb=&n=1&s_district=All"]
 			return
 		else:
 			soup = BeautifulSoup(result.group(), "html.parser")
@@ -107,10 +106,10 @@ class courseSpider:
 
 	def gatherTerms(self, subject = "CSCI", term = "201710"):
 		url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term=" + term + "&s_subj=" + subject + "&s_numb=&n=1&s_district=All"
-		if self.data == "":
-			self.data = self.gatherData(url)
+		if courseSpider.data == "":
+			courseSpider.data = self.gatherData(url)
 		pattern = re.compile('Term-\s*?(.*?\s)*?<\/', re.M)
-		result = re.search(pattern, self.data)
+		result = re.search(pattern, courseSpider.data)
 		soup = BeautifulSoup(result.group(), "html.parser")
 		titles = soup.option.text.split("\n")
 		current = soup.option
@@ -122,10 +121,9 @@ class courseSpider:
 
 	def gatherSubject(self, subject = "CSCI", term = "201710"):
 		url = "https://dalonline.dal.ca/PROD/fysktime.P_DisplaySchedule?s_term=" + term + "&s_subj=" + subject + "&s_numb=&n=1&s_district=All"
-		if self.data == "":
-			self.data = self.gatherData(url)
+		courseSpider.data = self.gatherData(url)
 		pattern = re.compile('Subject-\s*?(.*?\s)*?<\/', re.M)
-		result = re.search(pattern, self.data)
+		result = re.search(pattern, courseSpider.data)
 		soup = BeautifulSoup(result.group(), "html.parser")
 		titles = soup.option.text.split("\n")
 		current = soup.option
@@ -155,28 +153,39 @@ class courseSpider:
 		course = DalCourse()
 		course.title = soup.b.string
 		if "-" in soup.span.text:
-			date = soup.span.text.split("\n")
-			course.date = date[0] + date[2]
+			course.date = soup.span.text.split(": ")[1]
 		else:
-			date = soup.contents[2].span.text.split("\n")
-			course.date = date[0] + date[2]
+			course.date = soup.contents[2].span.text.split(": ")[1]
 		course.link = soup.a['href']
 		course.registerID = int(soup.tr.contents[3].string)
 		course.courseType = soup.tr.contents[7].string
 		course.credit = float(soup.tr.contents[9].string)
+		if soup.tr.contents[13].string == "C/D":
+			course.time = "C/D"
+			course.address = ""
+			course.maxStudent = soup.tr.contents[15].text
+			course.current = soup.tr.contents[17].text
+			course.available = soup.tr.contents[19].text
+			if soup.tr.contents[21].string != "\xa0":
+				course.wList = soup.tr.contents[33].text
+			else:
+				course.wList = 0
+			course.percentage = soup.tr.contents[23].font.string.split('\n')[0].replace(" ", "")
+			course.professor = "Staff"
+			return course
 		course.time = soup.tr.contents[23].contents[0]
 		course.address = soup.tr.contents[25].contents[0]
-		if soup.tr.contents[27].string != "OPEN":
-			course.maxStudent = int(soup.tr.contents[27].string)
+		if soup.tr.contents[27].text != "OPEN":
+			course.maxStudent = soup.tr.contents[27].text
 		else:
-			course.maxStudent = 9999
-		course.current = int(soup.tr.contents[29].string)
-		course.available = int(soup.tr.contents[31].string)
+			course.maxStudent = "9999"
+		course.current = soup.tr.contents[29].text# Problem occur when there're multiple lines in Commerce
+		course.available = soup.tr.contents[31].text
 		if soup.tr.contents[33].string != "\xa0":
-			course.wList = int(soup.tr.contents[33].string)
+			course.wList = soup.tr.contents[33].text
 		else:
 			course.wList = 0
-		course.percentage = soup.tr.contents[35].font.string.split('\n')[0]
+		course.percentage = soup.tr.contents[35].font.string.split('\n')[0].replace(" ", "")
 		try:
 			if ("\n" in soup.contents[7].text):
 				course.professor = soup.contents[7].text.split('\n')[1]
@@ -201,14 +210,18 @@ class courseSpider:
 				break
 		return course
 
+
 cs = courseSpider()
-subject = cs.subjects['Computer Science']
-print(subject)
-term = cs.terms['2016/2017 Winter']
+cs.gatherTerms()
+term = cs.terms['2015/2016 Summer']
 print(term)
+cs.gatherSubject(term = term)
+subject = cs.subjects['Chemical Engineering']
+print(subject)
 cs.spider(subject = subject, term = term)
-course = []
-for each in cs.courses:
-	course.append(each.toDict())
-courses = {"courses":course}
-print(json.dumps(courses))
+print(cs.courses)
+# course = []
+# for each in cs.courses:
+# 	course.append(each.toDict())
+# courses = {"courses":course}
+# print(json.dumps(courses))
